367d7860eb2d783a7676875f1a4837349a7bfe7c
ganeshwanare07 <ganeshwanare07@gmail.com>
2026-05-21 23:53:54 +0530

    Changes in sh file

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 cd "/Users/ganeshwanare/Desktop/Ganesh/new test data/Testing Notes Credence/Assignments/Automation_Practice/Test_Cases_And_Projects/Automation_Test_Cases_35"
 
 #Single browser with all test cases
-#pytest -v -s --browser chrome --alluredir=Reports --html=Html_Report/my_report.html
+pytest -v -s --browser chrome --alluredir=Reports --html=Html_Report/my_report.html
 
 #All Browsers with specific test case
 #pytest -v -s -n=auto --html=Html_reports\my_headless_report_31st_jan_2026.html --browser headless -k "test_verify_Credkart_url_001" --alluredir=AllureReports
 #pytest -n=auto --html=Html_reports\my_chromereport_31st_jan_2026.html --browser chrome -k "test_verify_Credkart_url_001" --alluredir=AllureReports
 #pytest -n=auto --html=Html_reports\my_firefoxreport_31st_jan_2026.html --browser firefox -k "test_verify_Credkart_url_001" --alluredir=AllureReports
-pytest -n=auto --html=Html_reports\my_safarireport_31st_jan_2026.html --browser safari -k "test_verify_Credkart_url_001" --alluredir=AllureReports
+#pytest -n=auto --html=Html_reports\my_safarireport_31st_jan_2026.html --browser safari -k "test_verify_Credkart_url_001" --alluredir=AllureReports
 
 #pytest -n=auto --html=Html_reports\my_edgereport_31st_jan_2026.html --browser edge -k "test_verify_Credkart_url_001" --alluredir=AllureReports
 
@@ -35,3 +35,8 @@ pytest -n=auto --html=Html_reports\my_safarireport_31st_jan_2026.html --browser 
 #Jenkins command
 #brew services list
 #brew services start jenkins-lts
+#brew services start jenkins-lts
+#brew services stop jenkins-lts
+#brew services restart jenkins-lts
+#brew services list
+#jenkins-lts started
